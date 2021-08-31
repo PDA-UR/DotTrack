6b386e1fc01de6d7a2d08c3bdaa3754b1752d01a
Andreas Schmid <andreas.schmid@ur.de>
2021-08-31 11:43:53 +0200

    Update config.hpp

--- DotTrack/src/config.hpp
+++ DotTrack/src/config.hpp
@@ -1,11 +1,3 @@
-
-//#define ssid "itHurtsWhenIP"
-//#define pw "JcPv9demunVW"
-//#define HOST_IP "192.168.178.77"
-
-#define ssid "sensor.uni-regensburg.de"
-#define pw "iot2019tb"
-#define HOST_IP "10.61.15.136" // AS
-
-//#define HOST_IP "10.61.1.212" // SL
-//#define HOST_IP "10.61.22.3" // TF
+#define ssid "wifi_ssid"
+#define pw "password"
+#define HOST_IP "1.2.3.4"
